57a53a8f9c4611f6be24d89e6ac5f15402a0bcb9
ksshim86 <31685526+ksshim86@users.noreply.github.com>
2017-11-26 17:20:48 +0900

    Share Project

---
+++ ShyaProject/src/main/java/com/sy/shya/ShyaProjectApplication.java
@@ -0,0 +1,12 @@
+package com.sy.shya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShyaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShyaProjectApplication.class, args);
+	}
+}
