e0846003bda423b15a2499cd4a9c091059ed0199
JiYoon Yang <wldbs1678@gmail.com>
2017-11-26 17:37:21 +0900

    @Id 추

--- ShyaProject/src/main/java/com/sy/shya/user/domain/User.java
+++ ShyaProject/src/main/java/com/sy/shya/user/domain/User.java
@@ -1,9 +1,11 @@
 package com.sy.shya.user.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class User {
+	@Id
 	private String id;
 	private String name;
 	private String password;
