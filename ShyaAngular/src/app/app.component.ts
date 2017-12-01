12bc7c9acbfaeec15417a5c3d3c9f123a86d9175
ksshim86 <31685526+ksshim86@users.noreply.github.com>
2017-12-01 22:34:05 +0900

    import httpClient

--- ShyaAngular/src/app/app.component.ts
+++ ShyaAngular/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { OnInit, OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
+
+  constructor( private http: HttpClient ) {
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.http.get( 'http://localhost:8080/users' ).subscribe(data => {
+      console.log( data );
+    })
+  }
+
+  ngOnDestroy() {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    
+  }
 }
